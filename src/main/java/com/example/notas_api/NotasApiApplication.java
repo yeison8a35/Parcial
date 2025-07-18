a4693e7c48bc862d415fed6f0141c8c136795da9
yeison8a35 <yeison8a35@gmail.com>
2025-07-18 16:13:28 -0500

    first commit

---
+++ src/main/java/com/example/notas_api/NotasApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.notas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notas_api/NotasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
